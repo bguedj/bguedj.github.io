90224f312bf2f811ae236d6fc149649802a3d0c1
Benjamin Guedj, Ph.D <benjamin.guedj@inria.fr>
2017-11-15 14:43:12 +0100

    teaching

--- teaching/script.R
+++ teaching/script.R
@@ -1,21 +1,64 @@
-library(data.table) 
-# matchdata<-fread("/data/pcollinet/matchdata.csv") 
-# StudentsData<-fread("/data/pcollinet/StudentsData.csv") 
-titanic<-fread("titanic.csv") 
+rm(list=ls())
 
-sapply(titanic ,function(x) sum(is.na(x)))
+train <- read.csv("Training50.csv")
+test <- read.csv("Test50.csv")
+train <- train[,-1]
+test <- test[,-1]
+library(tree)
+
+nTrain <- nrow(train)
+cart.pred <- tree(factor(Creditability)~. , data=train, control=tree.control(nTrain , mincut = 15, minsize = 30, mindev =0),split="deviance")
+plot(cart.pred)
+text(cart.pred, pretty=0, cex=0.5)
+
+predicted <- predict(cart.pred, newdata=test , type="class")
+print(table(predicted ,test$Creditability))
+
+# pruned <- prune.tree(cart.pred,k=2.3,method = "deviance")
+# predicted.pruned <- predict(pruned, newdata=test , type="class")
+pruned2 <- prune.tree(cart.pred , best=8,method = "deviance")
+predicted.pruned2 <- predict(pruned2, newdata=test , type="class")
+
+# print(table(predicted.pruned ,test$Creditability))
+print(table(predicted.pruned2 ,test$Creditability))
+
+# plot(pruned)
+# text(pruned)
+
+plot(pruned2)
+text(pruned2)
+
+## CV
+
+n <- dim(train)[1]
+V <- 5
+I <- array(0,dim=c(V,n/V))
+
+reste <- 1:n
+for(i in 1:V){
+  I[i,] <- sample(reste,n/V)
+  reste <- setdiff(reste, I[i,])
+}
+
+Ibest <- c(2:25)
+N <- length(Ibest)
+scor <- array(0,N)
+
+for(i in 1:V){
+  cv_valid <- train[I[i,],]
+  cv_train <- train[-I[i,],]
+  cart <- tree(factor(Creditability)~., data = cv_train)
+  for(k in 1:N){
+    b <- Ibest[k]
+    pruned <- prune.tree(cart, best=b, method="misclass")
+    predictions <- predict(pruned, newdata=cv_valid, type="class")
+    scor[k] <- scor[k]+mean(predictions != cv_valid$Creditability)
+  }
+}
 
 
-titanic$Age[is.na(titanic$Age)] <- mean(titanic$Age,na.rm=T)
-titanic <- titanic [!is.na(titanic$Embarked),]
-
-total <- 1:nrow(titanic)
-train <- sample(x=total,size=floor(.8*length(total)),replace=F)
-
-linmodel <- lm(Survived~.,data=titanic[train,])
-linmodel <- lm( Survived~Age + Sex , data=titanic[train,])
-
-require(MASS)
-Lambdas <-  c(seq(0.001,0.01,1),seq(1,2000,1))
-model_lin <- lm.ridge(Survived~.,data=titanic[train,],lambda=Lambdas)
-betasRidge <- coef(model_lin )
+require(ROCR)
+predictProba <- predict(cart.pred ,newdata=test ,type="vector")[ ,2]
+pr <- prediction(predictProba , test$Creditability)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
---
+++ teaching/scriptSVM.R
@@ -0,0 +1,52 @@
+## agrégation
+dat <- iris
+
+
+# https://archive.ics.uci.edu/ml/datasets/wine+quality
+
+
+rm(list=ls())
+
+prop <- .9
+
+# library(data.table)
+# matchdata<-fread("/data/pcollinet/matchdata.csv")
+# StudentsData<-fread("/data/pcollinet/StudentsData.csv")
+# StudentsData<-fread("StudentsData.csv")
+
+StudentsData <- read.csv("StudentsData.csv")
+# titanic<-fread("titanic.csv")
+
+pairs(StudentsData[,c("raisedhands","VisITedResources","Discussion")])
+
+idx <- sample(x=1:nrow(StudentsData),size=floor(prop*nrow(StudentsData)),replace=F)
+
+StudentsData[["Class"]] <- as.factor(StudentsData[["Class"]])
+
+train <- StudentsData[idx,]
+test <- StudentsData[-idx,]
+
+library (e1071)
+svm.fit <- svm(Class~., data=train,kernel="radial",gamma= 0.5, cost =1)
+
+obj <- tune.svm(Class~.,
+                data = train,
+                kernel = "polynomial",
+                degree = 1:10,
+                gamma=seq(0.1,2,0.1),
+                cost =2^(0:4),
+                tunecontrol=tune.control(sampling="fix",fix=2/3,cross=8)
+                )
+summary(obj)
+Best <- obj$best.parameters
+svm.optimized <- svm(Class~.,
+                     data=train ,
+                     kernel="polynomial",
+                     degree=Best$degree ,
+                     gamma=Best$gamma,
+                     cost=Best$cost)
+
+cbind(test[["Class"]],predict(svm.optimized,test))
+
+print(table(test[["Class"]],predict(svm.fit,test)))
+print(table(test[["Class"]],predict(svm.optimized,test)))
